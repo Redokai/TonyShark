b05803de17522fdbe6f36495ee0c703b8c376d9c
Petrônio de Barros Mesquita <petroniobmesquita@gmail.com>
2018-07-04 13:34:33 -0300

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=tonyshark-Web-Deploy.pubxml -p:Password=r5Buo95wxJzl36HBiT5fht4xsGGhTMnSCeSsp15kHNhDkBwRwNSjt2T8KWnM
+
